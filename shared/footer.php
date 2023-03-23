3fc6ec550e78860aee0e9a46cd811cad0ef86ef4
Hossam Ahmed Salama <121521931+HossSalama@users.noreply.github.com>
2023-03-23 06:50:40 +0200

    Add files via upload

---
+++ shared/footer.php
@@ -0,0 +1,11 @@
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>HR System</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      
+      Designed by <a>Hossam Salam</a>
+    </div>
+  </footer><!-- End Footer -->
